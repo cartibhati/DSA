0cc04a1b8daa186c3cdd9124cbb8ab4bb5b7d966
cartibhati <mymaimi12@gmail.com>
2026-03-06 22:52:02 +0530

    inital commit

---
+++ 03_Arrays/12_Valid_Anagrams_242.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+
+        for(char x : s){
+            mp1[x]++;
+        }
+        for(char y : t){
+            mp2[y]++;
+        }
+        if(mp1==mp2)
+        return true;
+
+        else
+        return false;
+    }
+};
