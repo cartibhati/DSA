6d4fecd36f3ed0ff1cd5f8703e7d49a757f95c3b
cartibhati <mymaimi12@gmail.com>
2026-03-08 23:24:06 +0530

    kadane algorithm

---
+++ 03_Arrays/16_Max_subarray_53.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currsum=0;
+        int maxsum=0;
+        for(int i=0;i<nums.size();i++){
+            currsum=max(nums[i],nums[i]+currsum);
+            maxsum=max(maxsum,currsum);
+            if(currsum<0){
+                currsum=0;
+            }
+        }
+        return maxsum;
+    }
+};
