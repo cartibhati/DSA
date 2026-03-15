e4ef13eaddf73210ec759329d4b49bb217764ac5
cartibhati <mymaimi12@gmail.com>
2026-03-15 21:44:33 +0530

    dp ques

---
+++ 03_Arrays/20_stock_by_and_sell.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int minprice=prices[0];
+        int n=prices.size();
+        for(int i=1;i<n;i++){
+            if(prices[i]<minprice){
+                minprice=prices[i];
+            }
+            int profit=prices[i]-minprice;
+
+            if(profit>maxi){
+                maxi=profit;
+            }
+        }
+        return maxi;
+
+    }
+};
