11bbe7c88d56019f703035046975938d969715e7
cartibhati <mymaimi12@gmail.com>
2026-05-26 15:25:54 +0530

    changes

--- 03_Arrays/40_remove_dup_from_sorted_array.cpp
+++ 03_Arrays/40_remove_dup_from_sorted_array.cpp
@@ -6,7 +6,7 @@ public:
         while(j<nums.size()){
             if(nums[j]!=nums[i-1]){
                 nums[i]=nums[j];
-                i++;
+                i++; 
             }
             j++;
         }
