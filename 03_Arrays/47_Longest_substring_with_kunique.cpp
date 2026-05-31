5d48abcdc14182635b2080b75f7b676578efb2d4
cartibhati <mymaimi12@gmail.com>
2026-05-31 12:44:11 +0530

    variable size sliding window

---
+++ 03_Arrays/47_Longest_substring_with_kunique.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestKSubstr(string &s, int k) {
+        int low = 0;
+        int n = s.size();
+        int ans = -1;
+
+        unordered_map<char, int> f;
+
+        for (int high = 0; high < n; high++) {
+            f[s[high]]++;
+
+            while (f.size() > k) {
+                
+                f[s[low]]--;
+                if (f[s[low]] == 0) {
+                    f.erase(s[low]);
+                }
+                low++;
+            }
+
+            if (f.size() == k) {
+                ans = max(ans, high - low + 1);
+            }
+        }
+
+        return ans;
+    }
+};
