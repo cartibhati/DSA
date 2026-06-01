eaddce516ad65c7bb0fc7bb2c558c4b2e1712652
cartibhati <mymaimi12@gmail.com>
2026-06-01 22:45:05 +0530

    sliding window variant

---
+++ 03_Arrays/50_Longest_repeating_char_replacment.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int find(vector<int> &a)
+    {
+    int maxc=-1;
+    for(int i=0;i<256;i++)
+    maxc=max(maxc,a[i]);
+    return maxc;
+    }
+
+    int characterReplacement(string s, int k) {
+        int low=0;
+        int high=0;
+        int res=INT_MIN;
+        int n=s.size();
+        vector<int> f(256,0);
+
+        for(high=0;high<n;high++){
+            f[s[high]]++;
+            int len=high-low+1;
+            int maxcnt = find(f);
+            int diff = len-maxcnt;
+
+            while(diff>k){
+                f[s[low]]--;
+                low++;
+                len = high-low+1;
+                maxcnt=find(f);
+                diff = len-maxcnt;
+            }
+            len = high-low+1;
+            res = max(res,len);
+        }
+       if(res==INT_MIN)
+       return 0;
+       return res;
+
+    }
+};
